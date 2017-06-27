6dc1014e307f3e3e7ed865093aa5dbd3fe1e94aa
Nadia Fermin Albritton <ferminnadia@gmail.com>
2017-06-27 05:33:23 -0400

    fixing API

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,12 @@ Rails.application.routes.draw do
 
     resources :searches, only: [:index]
 
+  end
+
+
+    namespace :api do
+      namespace :v1 do
+        resources :matches
+      end
     end
   end
